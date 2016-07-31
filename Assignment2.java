e6d8bb3a8a07cad30cd0364d89e683d346fa62b3
u174762 <sam.panga@hotmail.com>
2016-07-31 16:14:19 +0530

    Add files via upload
    java assignment2 session2

---
+++ Assignment2.java
@@ -0,0 +1,18 @@
+
+public class Assignment2 {
+
+public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		 int a=2,b,c=2;
+		  
+		  b=a+c;
+		  
+		  System.out.println(b);
+		  
+			}
+
+		
+
+	}
+
+
